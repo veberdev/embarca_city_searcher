c0f71c16d5164d3bebeeef8fb06926982756c48f
Weslley Lucas <weslusisa@gmail.com>
2024-10-09 17:00:39 -0300

    feat: add controller, view and implement working search functionality

--- app/controllers/cities_controller.rb
+++ app/controllers/cities_controller.rb
@@ -2,5 +2,22 @@
 
 # CitiesControllerController handles requests related to city resources.
 class CitiesController < ApplicationController
-  def search; end
+  def search
+    @cities = fetch_cities
+    respond_to do |format|
+      format.js
+      format.html
+    end
+  end
+
+  private
+
+  def fetch_cities
+    state = State.find_by(name: params[:state])
+    if params[:name].present?
+      City.where('state_id = ? AND name ILIKE ?', state.id, "%#{params[:name]}%")
+    else
+      City.where(state_id: State.first.id)
+    end
+  end
 end
