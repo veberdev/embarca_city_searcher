c249acdc60d270c9579c9c743b0831679e36ce6b
Weslley Lucas <weslusisa@gmail.com>
2024-10-09 17:31:14 -0300

    test: add tests for cities association destruction with State

--- spec/models/state_spec.rb
+++ spec/models/state_spec.rb
@@ -6,4 +6,15 @@ RSpec.describe State, type: :model do
   describe 'associations' do
     it { should have_many(:cities).dependent(:destroy) }
   end
+
+  describe 'destroying a state' do
+    let!(:parana) { create(:state) }
+    let!(:curitiba) { parana.cities.create! }
+    let!(:santa_catarina) { parana.cities.create!(name: 'Santa Catarina') }
+    it 'destroys associated cities when the state is deleted' do
+      expect do
+        parana.destroy
+      end.to change(City, :count).by(-2)
+    end
+  end
 end
